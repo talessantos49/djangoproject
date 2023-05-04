0106c7ae18135c5f3b0c7669cff482b095143a0f
talessantos49 <talessantos49@gmail.com>
2023-05-04 11:06:31 -0300

    Subida das informações

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.template),
     path('retificalonga/maq235', views.maq235),
     path('retificalonga/template', views.template),
     path('retificaorbital/maq15', views.maq15)
