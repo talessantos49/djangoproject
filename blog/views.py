d04de800b248a5f84c4ac11b01ea0c4b37be08f6
talessantos49 <talessantos49@gmail.com>
2023-05-04 14:08:13 -0300

    Mudança de pasta do service_account

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def template(request):
 def maq15(request):
     return render(request, "blog/maq15.html")
 
-gc = gspread.service_account(filename='/service_account.json')
+gc = gspread.service_account(filename='../service_account.json')
 sh = gc.open_by_key('1noVM0ci_yNDi7I2egsF2-erzuTgBLa9LrOrM4q96xCk')
 worksheet = sh.worksheet("Página1")
 
