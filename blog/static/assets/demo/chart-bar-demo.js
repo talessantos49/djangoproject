73d2e8e0db4060b668f83b956040d7e66095f144
talessantos49 <talessantos49@gmail.com>
2023-05-12 11:00:48 -0300

    Quantidade de peças por dia agora com informações sobre a data sendo atualizada diariamente

--- blog/static/assets/demo/chart-bar-demo.js
+++ blog/static/assets/demo/chart-bar-demo.js
@@ -2,17 +2,31 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+var data = new Date();
+var dia05 = new Date(data.getTime() - (0 * 24 * 60 * 60 * 1000));
+var dia04 = new Date(data.getTime() - (1 * 24 * 60 * 60 * 1000));
+var dia03 = new Date(data.getTime() - (2 * 24 * 60 * 60 * 1000));
+var dia02 = new Date(data.getTime() - (3 * 24 * 60 * 60 * 1000));
+var dia01 = new Date(data.getTime() - (4 * 24 * 60 * 60 * 1000));
+var dia00 = new Date(data.getTime() - (5 * 24 * 60 * 60 * 1000));
+dataAtual = dia05.toLocaleDateString('pt-BR', {day: '2-digit', month: '2-digit', year: 'numeric'});
+dataFive = dia04.toLocaleDateString('pt-BR', {day: '2-digit', month: '2-digit', year: 'numeric'});
+dataFour = dia03.toLocaleDateString('pt-BR', {day: '2-digit', month: '2-digit', year: 'numeric'});
+dataThree = dia02.toLocaleDateString('pt-BR', {day: '2-digit', month: '2-digit', year: 'numeric'});
+dataTwo = dia01.toLocaleDateString('pt-BR', {day: '2-digit', month: '2-digit', year: 'numeric'});
+dataOne = dia00.toLocaleDateString('pt-BR', {day: '2-digit', month: '2-digit', year: 'numeric'});
+
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["06/03/2023", "07/03/2023", "08/03/2023", "09/03/2023", "10/03/2023", "11/03/2023"],
+    labels: [dataOne, dataTwo , dataThree,dataFour, dataFive, dataAtual],
     datasets: [{
-      label: "Quantidade:",
+      label: "Quantidade ",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [1462, 1068, 1357, 1250, 1300, 1500],
     }],
   },
   options: {
@@ -31,8 +45,8 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
-          maxTicksLimit: 5
+          max: 1500,
+          maxTicksLimit: 6
         },
         gridLines: {
           display: true
